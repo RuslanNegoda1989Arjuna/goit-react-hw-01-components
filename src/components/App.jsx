d05c69a17a8e58c487f371df712c45165a95bea4
Ruslan Negoda <iarjuna108@gmail.com>
2022-10-23 18:45:42 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+
 export const App = () => {
-  console.log('Hello world');
+  console.log(user);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      ></Profile>
     </div>
   );
 };
