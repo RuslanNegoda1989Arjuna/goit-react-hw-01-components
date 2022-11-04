e21d4bfc5bb3189a3c9040aad217d92f68f8c0ad
Ruslan Negoda <iarjuna108@gmail.com>
2022-11-04 09:38:53 +0200

    after mistake

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,28 +2,10 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
-  console.log(title);
-  if (title) {
-    return (
-      <section className={css.statistics}>
-        <h2 className={css.title}>{title}</h2>
-
-        <ul className={css.statList}>
-          {stats.map(({ id, label, percentage }) => {
-            return (
-              <li className={`${css.item} ${css[id]}`} key={id}>
-                <span className={css.label}>{label}</span>
-                <span className={css.percentage}>{percentage}%</span>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    );
-  }
-
   return (
     <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => {
           return (
